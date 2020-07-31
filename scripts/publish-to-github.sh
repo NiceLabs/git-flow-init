ba7a0ae3025e8b233cb3936ff34e230660111884
Septs <github@septs.pw>
2020-07-31 16:14:54 +0800

    Update publish-to-github.sh

--- scripts/publish-to-github.sh
+++ scripts/publish-to-github.sh
@@ -11,5 +11,5 @@ jq '.name = "@nicelabs/git-flow-init"' package.json > package-modified.json
 mv package-modified.json package.json
 
 VERSION=$(jq -r '.version' package.json)
-npm --no-git-tag-version version "$VERSION-$GITHUB_RUN_NUMBER"
+npm --no-git-tag-version version "$VERSION-$BUILD_VERSION"
 npm publish
